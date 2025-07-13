7a536f5ae4fb051ab11c398a35b2f903325cb089
phili <0758510@cegepoutaouais.qc.ca>
2025-07-13 16:32:11 -0400

    BOUYYYAAAA Hes back!! À voir les relations et les clés sont-elles créées?!?!?

---
+++ ProjetFinal_Ecommerce/Migrations/20250713202218_customAppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetFinal_Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class customAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nom",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Prenom",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nom",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Prenom",
+                table: "AspNetUsers");
+        }
+    }
+}
--- ProjetFinal_Ecommerce/Models/AppUser.cs
+++ ProjetFinal_Ecommerce/Models/AppUser.cs
@@ -4,6 +4,17 @@ namespace ProjetFinal_Ecommerce.Models
 {
     public class AppUser : IdentityUser
     {
+        public AppUser() : base()
+        {
+            ListeFactures = new List<Facture>();
+        }
+        public AppUser(string userName) : base(userName)
+        {
+            ListeFactures = new List<Facture>();
+        }
+        public string? Nom { get; set; } = null;
+        public string? Prenom { get; set; } = null;
+        public string? NomComplet => $"{Nom}, {Prenom}" ?? null;
         public List<Facture>? ListeFactures { get; set; }
         public Facture? Facture { get; set; } // Navigation vers facture
     }
